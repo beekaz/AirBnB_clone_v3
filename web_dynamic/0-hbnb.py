d0180aa16a9cd1372de41ae95ed302de16428f96
Justine-ini <inijustine4040@gmail.com>
2024-01-13 09:55:59 -0800

    Import uuid

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 from models import storage
+import uuid
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
